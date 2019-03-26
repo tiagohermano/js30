ae6e2f3bb7038cd7e619f95acf3a2136241423d9
Tiago Hermano <tiagofhermano@gmail.com>
2019-03-26 14:20:06 -0300

    finish day 11 - Custom Video Player

---
+++ 11 - Custom Video Player/scripts.js
@@ -0,0 +1,73 @@
+// Get elements
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
+const fullscreenButton = player.querySelector('.fs_button');
+
+// Functions
+function togglePlay() {
+  if(video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+};
+
+function updateButton() {
+  const icon = this.paused ? '►' : '❚ ❚';
+  toggle.textContent = icon;
+};
+
+function skip() {
+  video.currentTime += parseFloat(this.dataset.skip);
+};
+
+function handleRangeUpdate() {
+  video[this.name] = this.value;
+};
+
+function handleProgress() {
+  const percent = (video.currentTime / video.duration ) * 100;
+  progressBar.style.flexBasis = `${percent}%`;
+};
+
+function scrub(e) {
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = scrubTime;
+};
+
+function requestFullScreen() {
+  if (video.requestFullscreen) {
+    video.requestFullscreen();
+  } else if (video.mozRequestFullScreen) {
+    video.mozRequestFullScreen();
+  } else if (video.webkitRequestFullscreen) {
+    video.webkitRequestFullscreen();
+  } else if (video.msRequestFullscreen) { 
+    video.msRequestFullscreen();
+  }
+}
+
+// Add event listeners
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
+
+toggle.addEventListener('click', togglePlay);
+
+skipButtons.forEach(button => button.addEventListener('click', skip));
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
+
+let mouseDown = false;
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', (e) =>  mouseDown && scrub(e));
+progress.addEventListener('mousedown', () => mouseDown = true);
+progress.addEventListener('mouseup', () => mouseDown = false);
+
+fullscreenButton.addEventListener('click', requestFullScreen);
