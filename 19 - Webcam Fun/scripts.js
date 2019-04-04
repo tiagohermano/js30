4d05a131dabe6d1745a34479ad9c3b3f95b5d1d4
Tiago Hermano <tiagofhermano@gmail.com>
2019-04-04 12:25:36 -0300

    Finish day 19 - Webcam Fun

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -26,6 +26,12 @@ function paintToCanvas() {
 
   return setInterval(() => {
     ctx.drawImage(video, 0, 0, width, height);
+    // take the pixels out
+    let pixels = ctx.getImageData(0, 0, width, height);
+    // mess with them
+    pixels = rgbSplit(pixels);
+    // put them back
+    ctx.putImageData(pixels, 0, 0);
   }, 16);
 }
 
@@ -39,8 +45,28 @@ function takePhoto() {
   const link = document.createElement('a');
   link.href = data;
   link.setAttribute('download', 'handsome');
-  link.textContent = 'Download Image';
+  link.innerHTML = `<img src="${data}" alt="Handsome Man"/>`;
   strip.insertBefore(link, strip.firstChild);
+}
+
+function redEffect(pixels) {
+  for(let i=0; i<pixels.data.length; i+=4) {
+    pixels.data[i+0] = pixels.data[i+0] + 100 // red
+    pixels.data[i+1] = pixels.data[i+1] - 50  // green
+    pixels.data[i+2] = pixels.data[i+2] * 0.5 // blue
+    // pixels.data[i+3] = pixels.data[i+0] + 100 // alpha
+  }
+  return pixels;
+}
+
+function rgbSplit(pixels) {
+  for(let i=0; i<pixels.data.length; i+=4) {
+    pixels.data[i - 150] = pixels.data[i+0] // red
+    pixels.data[i + 100] = pixels.data[i+1] // green
+    pixels.data[i - 150] = pixels.data[i+2] // blue
+    // pixels.data[i+3] = pixels.data[i+0] + 100 // alpha
+  }
+  return pixels;
 }
 
 getVideo();
