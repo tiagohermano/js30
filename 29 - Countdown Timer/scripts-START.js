c0238ce10b40e3e4449abf94671192eb9dfbd6a7
Tiago Hermano <tiagofhermano@gmail.com>
2019-05-22 14:38:23 -0300

    Finish day 29 - Countdown Timer

---
+++ 29 - Countdown Timer/scripts-START.js
@@ -0,0 +1,54 @@
+let countdown;
+const timerDisplay = document.querySelector('.display__time-left');
+const endTimeDisplay = document.querySelector('.display__end-time');
+const buttons = document.querySelectorAll('[data-time]');
+
+function timer(seconds) {
+  //clear any existing timers
+  clearInterval(countdown);
+
+  const now = Date.now();
+  const then = now + seconds * 1000;
+  displayTimeLeft(seconds);
+  displayEndTime(then);
+
+  countdown = setInterval(() => {
+    const secondsLeft = Math.round((then - Date.now()) / 1000);
+    // check if you should stop it
+    if (secondsLeft < 0) {
+      clearInterval(countdown);
+      return;
+    }
+    //display it
+    displayTimeLeft(secondsLeft);
+  }, 1000)
+}
+
+function displayTimeLeft(seconds) {
+  const minutes = Math.floor(seconds / 60);
+  const remainderSeconds = seconds % 60;
+  const display = `${minutes < 10 ? `0${minutes}` : minutes}:${remainderSeconds < 10 ? `0${remainderSeconds}` : remainderSeconds}`;
+  timerDisplay.textContent = display;
+  document.title = display;
+}
+
+function displayEndTime(timestamp) {
+  const end = new Date(timestamp);
+  const hours = end.getHours();
+  const minutes = end.getMinutes();
+  const display = `Be back at ${hours}:${minutes < 10 ? '0': ''}${minutes}`;
+  endTimeDisplay.textContent = display;
+}
+
+function startTimer() {
+  const seconds = parseInt(this.dataset.time);
+  timer(seconds);
+}
+
+buttons.forEach(button => button.addEventListener('click', startTimer));
+document.customForm.addEventListener('submit', function(e) {
+  e.preventDefault();
+  const mins = this.minutes.value;
+  timer(mins * 60);
+  this.reset();
+})
